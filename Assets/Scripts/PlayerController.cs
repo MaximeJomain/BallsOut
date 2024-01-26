5cbfd5eab362e73f2fcb5f182b5e6d25fbc9656c
MaximeJomain <maxime.jomain95@gmail.com>
2024-01-27 00:49:33 +0100

    add player camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,55 @@
+using Cinemachine;
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public new Rigidbody rigidbody;
-    public float movementSpeed;
-    public Vector2 input;
+    public CinemachineFreeLook freeLookCamera;
+    public float movementSpeed, cameraSpeed;
+    
+    
+    private Vector2 moveInput, lookInput;
+    private new Rigidbody rigidbody;
+    private Camera mainCamera;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
     }
     
     private void FixedUpdate()
     {
-        if (input != Vector2.zero)
+        if (moveInput != Vector2.zero)
         {
-            Vector3 movement = new Vector3(input.x, 0f, input.y);
-            rigidbody.velocity = movement * movementSpeed;
+            Quaternion targetRotation = Quaternion.Euler(0f, mainCamera.transform.rotation.eulerAngles.y, 0f);
+            Vector3 moveDirection = targetRotation * new Vector3(moveInput.x, 0f, moveInput.y);
+            
+            // Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y);
+            rigidbody.velocity = moveDirection * movementSpeed;
         }
     }
-    
+
+    private void Update()
+    {
+        if (lookInput != Vector2.zero)
+        {
+            Debug.Log("Look : " + lookInput);
+        
+            lookInput.x *= 180f; 
+            freeLookCamera.m_XAxis.Value += lookInput.x * cameraSpeed * Time.deltaTime;
+            freeLookCamera.m_YAxis.Value += lookInput.y * cameraSpeed * Time.deltaTime;
+        }
+    }
+
     public void Move(InputAction.CallbackContext context)
     {
-        input = context.ReadValue<Vector2>();
-        Debug.Log("Move : " + input);
+        moveInput = context.ReadValue<Vector2>();
+    }
+    
+    public void Look(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>().normalized;
     }
 }
