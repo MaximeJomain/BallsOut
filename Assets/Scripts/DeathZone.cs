5d2b3f00d8fe5983c02808fd54244f7a0505d6a2
Mickael-95 <72389929+Mickael-95@users.noreply.github.com>
2024-01-27 11:16:42 +0100

    Add Spawn, Death and Win Point in the first lvl

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -24,7 +24,6 @@ public class DeathZone : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Dead");
             GameObject playerGameObject = other.gameObject;
             Destroy(playerGameObject);
             StartCoroutine(RespawnPlayer());
