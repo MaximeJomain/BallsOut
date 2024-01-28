4cd58285c1c89bfe7555c7bc6bf002c5dcae6376
Mickael-95 <72389929+Mickael-95@users.noreply.github.com>
2024-01-28 13:23:49 +0100

    Update Scene 4 and Update Win and Spawn point

--- Assets/Scripts/Level4/Level4Manager.cs
+++ Assets/Scripts/Level4/Level4Manager.cs
@@ -5,16 +5,9 @@ using UnityEngine;
 public class Level4Manager : MonoBehaviour
 {
 
-    [SerializeField]
-    private GameObject particlesPrefab;
-
-    [SerializeField]
-    private GameObject particles;
-
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(particlesPrefab, particles.transform.position, particles.transform.rotation);
     }
 
     // Update is called once per frame
